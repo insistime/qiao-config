f80a47cea5f119329ab6350293b7b5d23e59ba54
uikoo9 <uikoo9@qq.com>
2020-08-28 15:43:59 +0800

    all

---
+++ test/all.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var q = require('../lib/qiao-config');
+
+var s = q.all();
+console.log(s);
--- test/set.js
+++ test/set.js
@@ -3,3 +3,5 @@
 var q = require('../lib/qiao-config');
 
 q.set('key', 'value');
+q.set('key', 'value1');
+q.set('key1', 'value');
